69e3e66cc2e1e70ff11b3fc55a264bb441aeeab4
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-10-03 20:44:10 +0100

    feat: added browsable API Session Authentication

--- taskly/urls.py
+++ taskly/urls.py
@@ -14,12 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 from users import router as users_api_router
 
+auth_api_urls = []
+
+if settings.DEBUG:
+    auth_api_urls.append(path(r'verify/', include('rest_framework.urls')))
+
 api_url_patterns = [
+    path(r"auth/", include(auth_api_urls)),
     path(r"accounts/", include(users_api_router.router.urls))
 ]
 
