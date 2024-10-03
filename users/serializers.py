9e088fe35929488270d235e4be4a728d44c91fb5
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-10-03 19:52:43 +0100

    feat: set up User Registration

--- users/serializers.py
+++ users/serializers.py
@@ -3,6 +3,21 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True, required=False)
+
     class Meta:
         model = User
-        fields = ["url", "id", "username", "email", "first_name", "last_name"]
+        fields = ["url", "id", "username", "email", "first_name", "last_name", "password"]
+        # extra_kwargs = {
+        #     'password': {'write_only': True, 'required': False}
+        # }
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        user = User.objects.create(**validated_data)
+        # user = self.Meta.model(**validated_data)
+        if password is not None:
+            user.set_password(password)
+        user.save()
+        return user
