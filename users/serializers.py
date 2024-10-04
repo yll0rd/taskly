bc277a5a2546a61f58dfa82fd32d04e36347c918
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-10-04 09:21:38 +0100

    feat: implemented password change functionality

--- users/serializers.py
+++ users/serializers.py
@@ -6,10 +6,11 @@ class UserSerializer(serializers.ModelSerializer):
 
     username = serializers.CharField(read_only=True)
     password = serializers.CharField(write_only=True, required=False)
+    old_password = serializers.CharField(write_only=True, required=False)
 
     class Meta:
         model = User
-        fields = ["url", "id", "username", "email", "first_name", "last_name", "password"]
+        fields = ["url", "id", "username", "email", "first_name", "last_name", "password", "old_password"]
         # extra_kwargs = {
         #     'password': {'write_only': True, 'required': False}
         # }
@@ -22,3 +23,18 @@ class UserSerializer(serializers.ModelSerializer):
             user.set_password(password)
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        try:
+            user = instance
+            password = validated_data.pop('password', None)
+            old_password = validated_data.pop('old_password', None)
+            if password is not None and old_password is not None:
+                if user.check_password(old_password):
+                    user.set_password(password)
+                else:
+                    raise Exception("Old password is incorrect")
+                user.save()
+        except Exception as err:
+            raise serializers.ValidationError({ 'info': err })
+        return super(UserSerializer, self).update(instance, validated_data)
