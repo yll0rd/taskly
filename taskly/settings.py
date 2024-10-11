e8834f0a6d47d001414137298851360edd6e8980
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-10-11 01:49:52 +0100

    making deployment ready - please work abeg - if you work, I will give you bon bon

--- taskly/settings.py
+++ taskly/settings.py
@@ -228,6 +228,8 @@ if USE_S3:
     }
 else:
     STATIC_URL = 'static/'
+    MEDIA_URL = "media/"
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
     # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # # Static files (CSS, JavaScript, Images)
